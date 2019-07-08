1084ec4b24ec2225c8a80153443878b22445e3e3
Andres Gonzalez <andresg9108@gmail.com>
2019-07-08 10:03:44 -0500

    Carousel

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,7 +1,12 @@
 $(function(){
-	let miSwiperSlider = configSwiperSlider('.carousel1',
-		3, 10,
+	let oSwiper = configSwiperSlider('.carousel1',
+		1, 10,
 		'.swiper-pagination',
 		'.swiper-button-next',
 		'.swiper-button-prev');
+
+	resizeSlider(oSwiper, 4, 3, 1);
+	$(window).resize(function(){
+		resizeSlider(oSwiper, 4, 3, 1);
+	});
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-function resize(miSwiperSlider){
-	resizeSlider(miSwiperSlider, 1, 1, 1);
-}
-
 function configSwiperSlider(selector, views, space, pag, btnNext, btnPrev){
 	var swiper = new Swiper(selector, {
 	  slidesPerView: views,
@@ -20,14 +16,14 @@ function configSwiperSlider(selector, views, space, pag, btnNext, btnPrev){
 	return swiper;
 }
 
-function resizeSlider(swiper, pc, tablet, movil){
+function resizeSlider(swiper, pc, tablet, mobile){
 	var windowWidth = window.innerWidth;
 	if(windowWidth >= 1000){
 		swiper.params.slidesPerView = pc;
 	}else if(windowWidth >= 770){
 		swiper.params.slidesPerView = tablet;
 	}else{
-		swiper.params.slidesPerView = movil;
+		swiper.params.slidesPerView = mobile;
 	}
 
 	swiper.update();
